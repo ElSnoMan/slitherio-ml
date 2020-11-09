b43c07cb11777e37c8a485a3f12eb4e690732aa3
Carlos Kidman <ckcarlosdesign@gmail.com>
2020-11-08 21:10:38 -0700

    Better control over how the mouse moves the snake

--- tests/test_slither.py
+++ tests/test_slither.py
@@ -1,20 +1,62 @@
 import time
+import random
+from pylenium.element import Element
+from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.common.action_chains import ActionChains
 from pylenium.driver import Pylenium
+
+# auto viewport: {1280, 898}
+# custom viewport: {800, 800}
+
+bounding_box_coordinates = {
+    'top_left': (0, 0),
+    'top_center': (400, 0),
+    'top_right': (800, 0),
+    'right_center': (800, 400),
+    'bottom_right': (800, 800),
+    'bottom_center': (400, 800),
+    'bottom_left': (0, 800),
+    'left_center': (0, 400),
+    'center': (400, 400)
+}
+
+
+def stats_container(py) -> Element:
+    container = py.wait(timeout=3, ignored_exceptions=[StaleElementReferenceException]
+                        ).until(lambda _: py.contains('Your rank:').parent())
+    return container
+
+
+def in_game(py) -> bool:
+    if 'opacity: -1;' in stats_container(py).get_attribute('style'):
+        return False  # because the stats are hidden so we are not in the game
+    else:
+        return True
 
 
 def test_slither_io(py: Pylenium, fake):
     py.visit('https://slither.io')
+    py.viewport(800, 800)
     py.get('#nick').type(fake.first_name())
     py.getx("//div[@id='playh']//div[contains(text(), Play)]").click()
-    in_game = True
-    while in_game:
+
+    game = True
+    first_turn = True
+    moves = list()
+    center = 400, 400
+
+    while game:
         time.sleep(1)
-        container = py.contains('Your rank:').parent()
-        if 'opacity: -1;' in container.get_attribute('style'):
-            in_game = False  # hidden, which means we are not in the game
+        if not in_game(py):
             break
         py.screenshot('state.png')
-        actions = ActionChains(py.webdriver)
-        actions.move_by_offset(200, 200).perform()
-        actions.move_by_offset(0, 0).perform()
+        if first_turn:
+            x, y = center
+            first_turn = False
+        else:
+            x = random.randint(-50, 50)
+            y = random.randint(-50, 50)
+        move = x, y
+        print(move)
+        moves.append(move)
+        ActionChains(py.webdriver).move_by_offset(x, y).perform()
